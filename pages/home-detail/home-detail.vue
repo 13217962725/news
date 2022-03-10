a1c06bc859b1797cb39bc9887bd5843e3dfd6f82
墨殇 <1651337573@qq.com>
2022-03-10 18:15:21 +0800

    评论列表功能

--- pages/home-detail/home-detail.vue
+++ pages/home-detail/home-detail.vue
@@ -23,8 +23,7 @@
     </view>
     <view class="detail-content">
       <view class="detail-html">
-        <!-- <u-parse :content="formData.content" :noData="noData"></u-parse> -->
-        内容
+        <u-parse :content="formData.content" :noData="noData"></u-parse>
       </view>
       <view class="detail-comment">
         <view class="comment-title">最新评论</view>
@@ -39,7 +38,7 @@
         <uni-icons type="compose" size="16" color="#f07373"></uni-icons>
       </view>
       <view class="detail-bottom_icons">
-        <view class="detail-bottom_icons-box">
+        <view class="detail-bottom_icons-box" @click="open">
           <uni-icons type="chat" size="22" color="#f07373"></uni-icons>
         </view>
         <view class="detail-bottom_icons-box" @click="likeTap(formData._id)">
@@ -89,6 +88,12 @@
     },
     onReady() {},
     methods: {
+      // 打开评论列表
+      open() {
+        uni.navigateTo({
+          url: '../detail-comments/detail-comments?id=' + this.formData._id
+        })
+      },
       // 点赞
       thumbsup(article_id) {
         this.setUpdateThumbs(article_id)
